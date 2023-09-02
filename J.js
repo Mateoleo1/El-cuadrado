0dce4f4ec679839ece5642a577d8027d4fbed205
Mateoleo1 <104026146+Mateoleo1@users.noreply.github.com>
2023-09-02 10:54:46 -0700

    Add files via upload

---
+++ J.js
@@ -0,0 +1,11 @@
+var Nosex=0;
+var Nosey=0;
+var Rightwristx=0;
+var Leftwristx=0;
+var difference=0;
+function setup() {
+video=createCapture(VIDEO);
+video.size(525,525);
+canvas=createCanvas(500,500);
+canvas.position(550,150);
+}
